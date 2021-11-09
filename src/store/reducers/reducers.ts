6c6a8c23ba92c7ce26da7c990b067dbf7c4545ea
JeppeGustafsson <email@example.com>
2021-11-09 10:19:36 +0100

    Fixed bad import

--- src/store/reducers/reducers.ts
+++ src/store/reducers/reducers.ts
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import matches from './matches';
 import hamsters from './hamsters';
 import topWinners from './topWinners';
-import topLosers from './topWinners';
+import topLosers from './topLosers';
 import cutest from './cutsest';
 
 export default combineReducers({
